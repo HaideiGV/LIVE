e0f083457f880e99e892a6369a4b051bffca064c
HaideiGV <HaideiGV@gmail.com>
2015-05-18 02:59:18 +0300

    Live is work! jquery

--- liveproject/urls.py
+++ liveproject/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.conf.urls import *
 from liveupdate.models import Update
+from liveupdate.views import update
 
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     #'django.views.generic',
     #url(r'^$', 'list_default.object_list', {'queryset': Update.objects.all()}),
+    #url(r'^$', 'django.views.generic', {'queryset': Update.objects.all()}),
+    url(r'^update/$', update),
     url(r'^admin/', include(admin.site.urls)),
     )
--- liveupdate/views.py
+++ liveupdate/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from liveupdate.models import Update
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render_to_response
 
-# Create your views here.
+def update(request):
+    object_list = Update.objects.all()
+    return render_to_response('update_list.html', {'object_list': object_list})
