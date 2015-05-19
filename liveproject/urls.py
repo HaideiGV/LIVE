31fa3e0f19e6bb670e9e47e0b5308383155ad0a9
HaideiGV <HaideiGV@gmail.com>
2015-05-19 17:31:03 +0300

    jquery work with 404

--- liveproject/urls.py
+++ liveproject/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.conf.urls import *
 from liveupdate.models import Update
 from liveupdate.views import update,scripts
+import liveupdate.views
 
 
 urlpatterns = patterns(
@@ -12,4 +13,5 @@ urlpatterns = patterns(
     url(r'^update/$', update),
     url(r'^admin/', include(admin.site.urls)),
     (r'^scripts/([^/]+)$', scripts),
+    (r'^updates-after/(?P<id>\d+)/$',liveupdate.views.updates_after),
     )
--- liveupdate/views.py
+++ liveupdate/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from liveupdate.models import Update
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render_to_response
+from django.core import serializers
 import os
 
 
@@ -12,3 +13,9 @@ def update(request):
 def scripts(request, name):
     data = open(os.path.join('media/js',name), 'rb').read()
     return HttpResponse(data)
+
+def updates_after(request, id):
+    response = HttpResponse()
+    response['Content-Type'] = "text/javascript"
+    response.write(serializers.serialize("json", Update.objects.filter(pk__gt=id)))
+    return response
